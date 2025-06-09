add48a92e459d8b608240ae129e5c2ce0a717883
relevan69 <userfixxx69@gmail.com>
2025-06-09 17:53:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ def game_process():
   while True:
     random_value = random.randint(MIN_VALUE, MAX_VALUE)
     try:
-      user_input = input(f"Угадай число от {MIN_VALUE} до {MAX_VALUE}: ");
+      user_input = input(f"Угадай число от {MIN_VALUE} до {MAX_VALUE}: ")
       os.system("cls || clear")
       if len(user_input) == 0:
-        print("Ты ничего не написал! :с");
+        print("Ты ничего не написал! :с")
         continue
       user_value = int(user_input);
       if user_value < MIN_VALUE or user_value > MAX_VALUE:
-        print(f"Число загадано в диапазоне от {MIN_VALUE} до {MAX_VALUE}! Будь внимательнее.");
+        print(f"Число загадано в диапазоне от {MIN_VALUE} до {MAX_VALUE}! Будь внимательнее.")
         continue
       if user_value == random_value:
-        print("Ты угадал число! Да ты везунчик!! :3");
+        print("Ты угадал число! Да ты везунчик!! :3")
       else:
-        print("А вот и не угадал! Повезет в следующий раз :^");
+        print("А вот и не угадал! Повезет в следующий раз :^")
     except ValueError:
-      print("Ты ввел что-то помимо числа!! Так нельзя! >:c");
+      print("Ты ввел что-то помимо числа!! Так нельзя! >:c")
 
 def main():
   game_process()
